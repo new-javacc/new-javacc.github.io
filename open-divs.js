d707edd464de5aa234f93c049506dd0803e4327a
mvchessclub <abcd3.141592653589793238462643@gmail.com>
2016-07-16 11:24:31 -0600

    toggle arrows in features

--- open-divs.js
+++ open-divs.js
@@ -1,5 +1,16 @@
+var down = new Array();
+
+window.onload = function() {
+	for (i = 0; i < document.getElementsByClassName("feat-info").length; i++) {
+		down[i] = false;
+	}
+}
+
 function toggleDiv(n) {
+	down[n - 1] = !down[n - 1];
 	var elems = document.getElementsByClassName("feat-info");
 	var elem = elems[n - 1];
 	$(elem).animate({height: "toggle"}, 500);
+	if (down[n - 1]) document.getElementsByClassName("arrow")[n - 1].innerHTML = "&#9660;";
+	else document.getElementsByClassName("arrow")[n - 1].innerHTML = "&#9654;";
 }
