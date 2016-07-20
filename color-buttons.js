14ab969342233189e16dbb10ed888e5573be1a3e
mvchessclub <abcd3.141592653589793238462643@gmail.com>
2016-07-19 17:20:10 -0700

    updates...

--- color-buttons.js
+++ color-buttons.js
@@ -1,12 +1,12 @@
 var current_color = 0;
 var buttons = document.getElementsByClassName("thebutton");
 
-window.onload = function() {
-	var incr = 255 / buttons.length | 0;
-	var current = 0;
-	incr /= 1.5;
-	for (i = 0; i < buttons.length; i++) {
-		buttons[i].style.backgroundColor = "rgb(" + current + "," + current + ",255)";
-		current += incr;
-	}
-}
+// window.onload = function() {
+// 	var incr = 255 / buttons.length | 0;
+// 	var current = 0;
+// 	incr /= 1.5;
+// 	for (i = 0; i < buttons.length; i++) {
+// 		buttons[i].style.backgroundColor = "rgb(" + current + "," + current + ",255)";
+// 		current += incr;
+// 	}
+// }
