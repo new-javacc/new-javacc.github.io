28f7e9b3a468e7594f1d804de1e7f26301de5525
chess123 <kesav@viswanadha.net>
2017-01-25 14:44:42 -0800

    gottem

--- expand-picture.js
+++ expand-picture.js
@@ -2,7 +2,5 @@ expand();
 
 function expand() {
 	var h1 = document.getElementById("alldropdowns").clientHeight;
-	var h2 = document.getElementById("logo").clientHeight;
-	console.log(h1, h2);
-	document.getElementById("logo").style.paddingBottom = h1 - h2 + "px";
+	document.getElementById("logo").style.height = h1 + "px";
 }
