278f07b34ae93893aabbd10dd9692787b8fa34f9
chess123 <kesav@viswanadha.net>
2017-01-25 14:44:03 -0800

    fix 2

--- expand-picture.js
+++ expand-picture.js
@@ -3,5 +3,6 @@ expand();
 function expand() {
 	var h1 = document.getElementById("alldropdowns").clientHeight;
 	var h2 = document.getElementById("logo").clientHeight;
+	console.log(h1, h2);
 	document.getElementById("logo").style.paddingBottom = h1 - h2 + "px";
 }
